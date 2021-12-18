c118e2ed86ac33b2372121bbb9dfe5660f4ca344
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-12-18 09:42:08 -0400

    Update

---
+++ install.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+set -e
+
+########################################################
+# 
+#         Pterodactyl-AutoAddons Installation
+#
+#         Created and maintained by Ferks-FK
+#
+#            Protected by GPL 3.0 License
+#
+########################################################
+
+SCRIPT_VERSION="v1.0"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+reset="\e[0m"
+red='\033[0;31m'
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+
+#### Check Sudo ####
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+
+#### Check Curl ####
+
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+cancel() {
+echo
+echo -e "* ${red}Installation Canceled!${reset}"
+done=true
+exit 1
+}
+
+done=false
+
+echo
+print_brake 70
+echo "* Pterodactyl-AutoAddons Script @ $SCRIPT_VERSION"
+echo
+echo "* Copyright (C) 2021 - 2021, Ferks-FK."
+echo "* https://github.com/Ferks-FK/Pterodactyl-AutoAddons"
+echo
+echo "* This script is not associated with the official Pterodactyl Project."
+print_brake 70
+echo
+
+Backup() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/${SCRIPT_VERSION}/backup.sh)
+}
+
+More_Buttons() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/${SCRIPT_VERSION}/addons/version1.x/More_Buttons/build.sh)
+}
+
+More_Server_Info() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/${SCRIPT_VERSION}/addons/version1.x/More_Server_Info/build.sh)
+}
+
+
+while [ "$done" == false ]; do
+  options=(
+    "Restore Panel Backup (Only if you have an installation problem.)"
+    "Install More_Buttons (Only 1.6.6)"
+    "Install More_Server_Info (Only 1.6.6)"
+    
+    
+    "Cancel Installation"
+  )
+  
+  actions=(
+    "Backup"
+    "More_Buttons"
+    "More_Server_Info"
+    
+    
+    "cancel"
+  )
+  
+  echo "* Which addon do you want to install?"
+  echo
+  
+  for i in "${!options[@]}"; do
+    echo "[$i] ${options[$i]}"
+  done
+  
+  echo
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+  
+  [ -z "$action" ] && error "Input is required" && continue
+  
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
+done
