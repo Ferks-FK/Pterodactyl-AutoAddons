2221cb22781783d0e7b53c918e3390ab4bec4687
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-12-18 07:27:33 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Pterodactyl-AutoAddons
- Script for automatic installation of addons for the Pterodactyl 1.x panel
+<h1 align="center"> 
+    Pterodactyl-AutoThemes
+</h1>
+</br>
+
+This is a addons installation script for the [Pterodactyl Panel](https://github.com/pterodactyl/panel).<br>
+The script installs addons that are compatible with their respective panel versions.
+
+![1](https://user-images.githubusercontent.com/69549678/135567472-4be8d459-1ce1-4eb8-92ab-758eba37a563.PNG)
+
+
+
+
+
+# Support
+
+If you have any problems with the script, ask for support in our official group [Pterodacyl-AutoThemes Support](https://discord.gg/buDBbSGJmQ)
+
+# How to use
+Just run the following command as root user.
+
+```
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/main/install.sh)
+```
+## Attention!
+*Do not run the command using sudo.*
+
+**Example:** ```$ sudo bash <(curl -s...```
+
+*You must be logged into your system as root to use the command.*
+
+**Example:** ```# bash <(curl -s...```
---
+++ addons/version1.x/More_Server_Info/build.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+set -e
+
+########################################################
+# 
+#         Pterodactyl-AutoAddons Installation
+#
+#         Created and maintained by Ferks-FK
+#
+#            Protected by GPL 3.0 License
+#
+########################################################
+
+#### Variables ####
+SCRIPT_VERSION="v1.0"
+SUPPORT_LINK="https://discord.gg/buDBbSGJmQ"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+red='\033[0;31m'
+reset="\e[0m"
+
+
+#### OS check ####
+
+check_distro() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$(echo "$ID")
+    OS_VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    OS_VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$(echo "$DISTRIB_ID")
+    OS_VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    OS="debian"
+    OS_VER=$(cat /etc/debian_version)
+  elif [ -f /etc/SuSe-release ]; then
+    OS="SuSE"
+    OS_VER="?"
+  elif [ -f /etc/redhat-release ]; then
+    OS="Red Hat/CentOS"
+    OS_VER="?"
+  else
+    OS=$(uname -s)
+    OS_VER=$(uname -r)
+  fi
+
+  OS=$(echo "$OS")
+  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+}
+
+#### Verify Compatibility ####
+
+compatibility() {
+echo
+print_brake 57
+echo -e "* ${GREEN}Checking if the addon is compatible with your panel...${reset}"
+print_brake 57
+echo
+sleep 2
+DIR="/var/www/pterodactyl/config/app.php"
+CODE="    'version' => '1.6.6',"
+if [ -f "$DIR" ]; then
+  VERSION=$(cat "$DIR" | grep -n ^ | grep ^12: | cut -d: -f2)
+    if [ "$VERSION" == "$CODE" ]; then
+        echo
+        print_brake 23
+        echo -e "* ${GREEN}Compatible Version!${reset}"
+        print_brake 23
+        echo
+      else
+        echo
+        print_brake 24
+        echo -e "* ${red}Incompatible Version!${reset}"
+        print_brake 24
+        echo
+        exit 1
+    fi
+  else
+    echo
+    print_brake 26
+    echo -e "* ${red}The file doesn't exist!${reset}"
+    print_brake 26
+    echo
+    exit 1
+fi
+}
+
+
+#### Install Dependencies ####
+
+dependencies() {
+echo
+print_brake 30
+echo -e "* ${GREEN}Installing dependencies...${reset}"
+print_brake 30
+echo
+case "$OS" in
+debian | ubuntu)
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs && sudo apt-get install -y zip
+;;
+esac
+
+if [ "$OS_VER_MAJOR" == "7" ]; then
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo yum install -y nodejs yarn && sudo yum install -y zip
+fi
+
+if [ "$OS_VER_MAJOR" == "8" ]; then
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo dnf install -y nodejs && sudo dnf install -y zip
+fi
+}
+
+
+#### Panel Backup ####
+backup() {
+echo
+print_brake 32
+echo -e "* ${GREEN}Performing security backup...${reset}"
+print_brake 32
+if [ -f "/var/www/pterodactyl/PanelBackup/PanelBackup.zip" ]; then
+echo
+print_brake 45
+echo -e "* ${GREEN}There is already a backup, skipping step...${reset}"
+print_brake 45
+echo
+else
+cd /var/www/pterodactyl
+mkdir -p PanelBackup
+zip -r PanelBackup.zip app config public resources routes storage database .env tailwind.config.js
+mv PanelBackup.zip PanelBackup
+fi
+}
+
+
+#### Donwload Files ####
+download_files() {
+print_brake 25
+echo -e "* ${GREEN}Downloading files...${reset}"
+print_brake 25
+cd /var/www/pterodactyl
+mkdir -p temp
+cd temp
+curl -sSLo More_Server_Info.tar.gz https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/addons/version1.x/More_Server_Info/More_Server_Info.tar.gz
+tar -xzvf More_Server_Info.tar.gz
+cd More_Server_Info
+cp -rf -- * /var/www/pterodactyl
+cd
+cd /var/www/pterodactyl
+rm -rf temp
+}
+
+#### Panel Production ####
+
+production() {
+DIR=/var/www/pterodactyl
+
+if [ -d "$DIR" ]; then
+echo
+print_brake 25
+echo -e "* ${GREEN}Producing panel...${reset}"
+print_brake 25
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn build:production
+fi
+}
+
+
+bye() {
+print_brake 50
+echo
+echo -e "* ${GREEN}The addon ${YELLOW}More Server Info${GREEN} was successfully installed."
+echo -e "* A security backup of your panel has been created."
+echo -e "* Thank you for using this script."
+echo -e "* Support group: ${YELLOW}$(hyperlink "$SUPPORT_LINK")${reset}"
+echo
+print_brake 50
+}
+
+
+#### Exec Script ####
+check_distro
+compatibility
+dependencies
+backup
+download_files
+production
+bye
